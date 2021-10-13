48b5aecef5e79a2538ce8ce387b6642eb2b6ce22
Ford Fishman <fordfishman@gmail.com>
2021-10-13 16:19:01 -0400

    cumulative distribution plot

--- r/testMutation.R
+++ r/testMutation.R
@@ -1,14 +1,17 @@
 library(ggplot2)
 
-df1 <- read.csv('~/GitHub/CommunityImmunity/output/test_100sims_t1000/summary.csv')
+# df1 <- read.csv('~/GitHub/CommunityImmunity/output/test_100sims_t1000/summary.csv')
+# df1 <- read.csv('~/GitHub/CommunityImmunity/output/test_100sims_t10000/summary.csv')
+df1 <- read.csv('~/GitHub/CommunityImmunity/output/20211013_0856_1000sims_t5000/summary.csv')
 df2 <- read.csv("~/GitHub/CommunityImmunity/output/test_1sims_t10000/param_evolve.csv")
 
 final_rate <- df1$m_final
 
 hist(log10(final_rate))
 boxplot(log10(final_rate))
+plot(ecdf(log10(final_rate))) # CPD
 
-p1 <- ggplot(NULL, aes(x=final_rate)) +
+p1 <- ggplot(NULL, aes(x=log10(final_rate))) +
   geom_histogram() +
   xlab('Final Average Mutation rate') +
   theme(panel.grid.major = element_blank(),
@@ -16,7 +19,7 @@ p1 <- ggplot(NULL, aes(x=final_rate)) +
         panel.background = element_blank(),
         axis.line = element_line(colour = "black"),
         axis.ticks.length = unit(5,"pt"),)
-ggsave(plot = p1, filename = '~/20210324_labmeetingp1.png', width = 7, height = 5)
+# ggsave(plot = p1, filename = '~/20210324_labmeetingp1.png', width = 7, height = 5)
 
 p2 <- ggplot(df2, aes(x=X, y=m)) +
   geom_line() +
@@ -28,4 +31,4 @@ p2 <- ggplot(df2, aes(x=X, y=m)) +
         axis.line = element_line(colour = "black"),
         axis.ticks.length = unit(5,"pt"),)
 
-ggsave(plot = p2, filename = '~/20210324_labmeetingp2.png', width = 7, height = 5)
+# ggsave(plot = p2, filename = '~/20210324_labmeetingp2.png', width = 7, height = 5)
